656a2a3cf15b58693e9d6c1c2f0249716da082e7
Xerrath <mccall.alan.dee@gmail.com>
2022-12-14 22:49:21 -0700

    init

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -54,16 +54,13 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div>
-        <h2>{this.state.pageTitle}</h2>
+        <div className="portfolio-items-wrapper">
+          <button className="btn" onClick={() => this.handleFilter ("Class")}>Class Projects</button>
+          <button className="btn" onClick={() => this.handleFilter ("Fitness")}>Fitness</button>
+          <button className="btn" onClick={() => this.handleFilter ("Gaming")}>Gaming</button>
 
-        <button onClick={() => this.handleFilter ("Class")}>Class Projects</button>
-        <button onClick={() => this.handleFilter ("Fitness")}>Fitness</button>
-        <button onClick={() => this.handleFilter ("Gaming")}>Gaming</button>
-
-        {this.portfolioItems()}
-        
-      </div>
+          {this.portfolioItems()}
+        </div>
     );
   }
 }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React , { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function(props) {
-    const { id, description, thumb_image_url, logo } = props.item;
-    return (
-        <div>
-            <div>{description}</div>
-            <img src={thumb_image_url} />
-            <img src={logo} />
-            <Link to={`/portfolio/${props.slug}`}>Link</Link>
-        </div>
-    );
+export default class PortfolioItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            portfolioItemClass: ""
+        };
+    }
+
+    handleMouseEnter() {
+        this.setState({portfolioItemClass: "image-blur"});
+    }
+
+    handleMouseLeave() {
+        this.setState({portfolioItemClass:""});
+    }
+
+    render() {
+        const { id, description, thumb_image_url, logo_url } = this.props.item;
+        return (
+            <div className='portfolio-item-wrapper'
+                onMouseEnter={() => this.handleMouseEnter()}
+                onMouseLeave={() => this.handleMouseLeave()}
+            >
+
+                <div
+                    className={'portfolio-img-background ' + this.state.portfolioItemClass}
+                    style={{
+                        backgroundImage: "url(" + thumb_image_url + ")"
+                    }}
+                />
+
+                <div className='img-text-wrapper'>
+
+                    <div class="logo-wrapper">
+                        <img src={logo_url} />
+                    </div>
+
+                    <div class="subtitle">
+                        {description}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
