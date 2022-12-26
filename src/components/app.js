70d735e7f349b0af5931fa2e8acda379abeaac74
Xerrath <mccall.alan.dee@gmail.com>
2022-12-26 03:25:06 -0700

    forms

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTrash,
-  faSignOutAlt,
-  faEdit,
-  faSpinner,
-  faCirclePlus
-} from "@fortawesome/free-solid-svg-icons";
+
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
@@ -21,11 +14,13 @@ import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
+import Icons from "../helpers/icons";
 
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faCirclePlus);
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    Icons();
 
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN"
