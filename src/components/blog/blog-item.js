4f3198b7c8531fbdf835297649f04af23cc6e91c
Xerrath <mccall.alan.dee@gmail.com>
2022-12-26 21:11:04 -0700

    modal update

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,25 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import striptags from "striptags";
+import Truncate from "react-truncate";
 
 const BlogItem = props => {
-    const {
+  const {
     id,
     blog_status,
     content,
     title,
     featured_image_url
-    } = props.blogItem;
+  } = props.blogItem;
 
   return (
     <div>
-        <Link to={`/b/${id}`}>
-            <h1>
-                {title}
-            </h1>
-        </Link>
-        <div>
-            {content}
-        </div>
+      <Link to={`/b/${id}`}>
+        <h1>{title}</h1>
+      </Link>
+      <div>
+        <Truncate
+          lines={5}
+          ellipsis={
+            <span>
+              ...<Link to={`/b/${id}`}>Read more</Link>
+            </span>
+          }
+        >
+          {striptags(content)}
+        </Truncate>
+      </div>
     </div>
   );
 };
